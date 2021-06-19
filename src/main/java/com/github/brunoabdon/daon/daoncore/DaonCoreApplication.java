33e31837210067e20388c2fb3a5fed6d1b186833
Bruno Abdon <brunoabdon+github@gmail.com>
2021-06-19 20:40:01 -0300

    Inicio pelo Spring Initializr"

---
+++ src/main/java/com/github/brunoabdon/daon/daoncore/DaonCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.brunoabdon.daon.daoncore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaonCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaonCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/brunoabdon/daon/daoncore/DaonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brunoabdon.daon.daoncore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaonCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
