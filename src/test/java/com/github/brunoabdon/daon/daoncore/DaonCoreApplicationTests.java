1e8f264bf112db64e4ab612c3f585c562cfa0d88
Bruno Abdon <brunoabdon+github@gmail.com>
2021-06-21 21:42:25 -0300

    Seta profile pra testes

--- src/test/java/com/github/brunoabdon/daon/daoncore/DaonCoreApplicationTests.java
+++ src/test/java/com/github/brunoabdon/daon/daoncore/DaonCoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.brunoabdon.daon.daoncore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class DaonCoreApplicationTests {
 
 	@Test
